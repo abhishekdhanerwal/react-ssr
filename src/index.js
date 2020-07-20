14128ba440f3a6089935815687553d416ab3d242
AzureAD\AbhishekKumar <abhishek@monocept.com>
2020-07-20 15:52:50 +0530

    test changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Routes from './client/Routes';
 import compression from 'compression';
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
